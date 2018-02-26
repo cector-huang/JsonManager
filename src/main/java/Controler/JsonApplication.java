78622ed040c2db28b32b6e37e0e911ac956d9ca1
cector-huang <huangjiaqi@huangjiaqideMacBook-Pro.local>
2018-02-26 22:13:40 +0800

    rename

--- src/main/java/Controler/JsonApplication.java
+++ src/main/java/Controler/JsonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsonDemo
+public class JsonApplication
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(JsonDemo.class, args);
+        SpringApplication.run(JsonApplication.class, args);
     }
 }
